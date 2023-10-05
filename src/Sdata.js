66b0821407188e2cb07585ff391d0096ac1f0439
MerajAhemad13 <mearajali007@gmail.com>
2023-10-05 10:48:57 +0530

    first commit

---
+++ src/Sdata.js
@@ -0,0 +1,35 @@
+import web from "../src/images/web.png";
+import app from "../src/images/app.jpg";
+import android from "../src/images/android.png";
+import digital from "../src/images/digital.jpg";
+import software from "../src/images/software.jpg";
+import marketinng from "../src/images/marketing.png";
+
+const Sdata = [
+  {
+    imgsrc: web,
+    title: "Web devlopment",
+  },
+  {
+    imgsrc: app,
+    title: "App devlopment",
+  },
+  {
+    imgsrc: software,
+    title: "Software devlopment",
+  },
+  {
+    imgsrc: digital,
+    title: "digital marketing",
+  },
+  {
+    imgsrc: android,
+    title: "Android devlopment",
+  },
+  {
+    imgsrc: marketinng,
+    title: "Marketinng",
+  },
+];
+
+export default Sdata;
